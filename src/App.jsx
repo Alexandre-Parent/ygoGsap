1883e6ac9484609c42e7c836cff831b32fb1f809
Alexandre Parent <alexp90300@gmail.com>
2025-11-17 11:00:14 +0100

    Setup de base

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-3xl text-violet-300 font-circular-web font-bold underline'>Hello World</h1>
+    </main>
+  )
+}
+
+export default App
