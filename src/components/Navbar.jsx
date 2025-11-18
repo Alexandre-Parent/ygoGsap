aeeac2e3acc42011d414e4150f095d43657277c3
Alexandre Parent <alexp90300@gmail.com>
2025-11-18 11:20:41 +0100

    changement de logo + fixbug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,11 +58,11 @@ const Navbar = () => {
         }
     }, [isAudioPlaying])
   return (
-    <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-99 h-16 border-none transition-all duration-700 sm:inset-x-6'>
-        <header className='absolute top-50% w-full'>
-            <nav className='flex size-full items-center justify-between p-4'>
+    <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-99 h-24 border-none transition-all duration-700 sm:inset-x-6 sm:h-20'>
+        <header className='flex h-full w-full items-center'>
+            <nav className='flex w-full items-center justify-between px-4'>
                 <div className='flex items-center gap-7'>
-                    <img src="/img/logo.png" alt="logo" className='w-10'/>
+                    <img src="/img/logo.png" alt="logo" className='w-24 sm:w-28'/>
                     <Button id='product-button' title='Produits' containerClass='bg-blue-50 md:flex hidden items-center justify-center gap-1'/>
                 </div>
                 <div className='flex h-full items-center'>
